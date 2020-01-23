92aabad8bdfbedc38b56bb230c7396a0f4df56f5
Jovin Delfin <jovdelfin01@gmail.com>
2020-01-24 00:19:16 +0800

    Commited Changes to Nav Drawer, Login Layout

--- app/src/main/java/com/elevintech/motorbro/Dashboard/DashboardActivity.kt
+++ app/src/main/java/com/elevintech/motorbro/Dashboard/DashboardActivity.kt
@@ -2,16 +2,20 @@ package com.elevintech.motorbro.Dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.forEach
 import androidx.fragment.app.FragmentTransaction
 import com.elevintech.motorbro.Dashboard.Fragments.*
 import com.elevintech.myapplication.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.drawer_dashboard.*
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var homeFragment: HomeFragment
     lateinit var partsFragment: PartsFragment
@@ -21,9 +25,78 @@ class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.drawer_dashboard)
 
+        buildNavigationDrawer()
         setUpBottomNav()
+    }
+
+    // Navigation Drawer
+    private fun buildNavigationDrawer(){
+
+        // set toolbar as our created view
+        setSupportActionBar(toolbar)
+
+        // remove default title, so we can set it through XML
+        getSupportActionBar()!!.setDisplayShowTitleEnabled(false)
+
+        val drawerLayout = drawer_layout
+        println("is layout nil")
+        println(drawerLayout)
+
+        // create navigation drawer
+        var toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+//        // get menu from navigationView
+//        val nav_menu = nav_view.menu
+//
+//        // find MenuItem you want to change
+//        val nav_profile_status = nav_menu.findItem(R.id.profile_status)
+//
+//        // set new title to the MenuItem
+//        nav_profile_status.title = "NewTitleForCamera"
+//
+//        // do the same for other MenuItems
+//        val nav_meeting = nav_menu.findItem(R.id.next_meeting)
+//        nav_meeting.title = "NewTitleForGallery"
+//
+//        // get header from navigationView
+//        val nav_header = nav_view.getHeaderView(0)
+//
+//        // find headerItem you want to change
+//        val nav_user = nav_header.user_first_name
+//
+//        // set text to the headerItem
+//        nav_user.setText("Gotcha")
+//
+//        val nav_user_image = nav_header.profileImage
+
+
+//        updateUserImage(nav_user_image)
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+    }
+
+
+
+    // On select of Navigation Drawer Menu
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        println("${item.itemId}")
+
+        when(item.itemId){
+
+
+            R.id.sign_out -> {
+                println("Logged Out")
+            }
+
+        }
+
+        return true
     }
 
     private fun setUpBottomNav() {
