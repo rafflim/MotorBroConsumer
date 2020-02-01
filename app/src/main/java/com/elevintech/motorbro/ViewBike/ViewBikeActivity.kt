04a4c2a709fbe489de3f6157330184ff21eb77f0
Jovin Delfin <jovdelfin01@gmail.com>
2020-02-01 20:08:42 +0800

    Made Changes for Garage, back buttons

---
+++ app/src/main/java/com/elevintech/motorbro/ViewBike/ViewBikeActivity.kt
@@ -0,0 +1,13 @@
+package com.elevintech.motorbro.ViewBike
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elevintech.myapplication.R
+
+class ViewBikeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_bike)
+    }
+}
