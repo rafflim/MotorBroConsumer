a09d778b1f6713b34204450ff8df44cb18824fa8
Lorrenz Cruz <john26cruz@gmail.com>
2020-03-26 07:48:13 +0800

    Set notification on click go to intent

--- app/src/main/java/com/elevintech/motorbro/ScheduledNotification/Receiver.kt
+++ app/src/main/java/com/elevintech/motorbro/ScheduledNotification/Receiver.kt
@@ -2,10 +2,12 @@ package com.elevintech.motorbro.ScheduledNotification
 
 import android.app.Notification
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
+import com.elevintech.motorbro.AddOdometer.AddOdometerActivity
 import com.elevintech.myapplication.R
 
 class Receiver: BroadcastReceiver(){
@@ -20,14 +22,21 @@ class Receiver: BroadcastReceiver(){
 
         println("Alarm Triggered!")
 
+        val intent = Intent(p0, AddOdometerActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(p0, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
         var builder = NotificationCompat.Builder(p0!!, channelId)
             .setSmallIcon(R.drawable.motorbroicon)
             .setContentTitle(notificationTitle)
             .setContentText(notificationText)
             .setDefaults(Notification.DEFAULT_VIBRATE) //Important for heads-up notification
             .setPriority(Notification.PRIORITY_MAX) //Important for heads-up notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .setOngoing(true)
 
         val manager = p0!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
         manager.notify(0, builder.build())
 
 
