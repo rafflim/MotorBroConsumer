c60e23e3ed5bd4fcd559426e820e54a5148387db
Lorrenz Cruz <john26cruz@gmail.com>
2020-04-27 23:04:24 +0800

    Finally fixed the alarm - add checker so it only display the odometer update notification IF the time is the SET time

--- app/src/main/java/com/elevintech/motorbro/ScheduledNotification/Receiver.kt
+++ app/src/main/java/com/elevintech/motorbro/ScheduledNotification/Receiver.kt
@@ -9,6 +9,7 @@ import android.content.Intent
 import androidx.core.app.NotificationCompat
 import com.elevintech.motorbro.AddOdometer.AddOdometerActivity
 import com.elevintech.myapplication.R
+import java.util.*
 
 class Receiver: BroadcastReceiver(){
 
@@ -18,27 +19,47 @@ class Receiver: BroadcastReceiver(){
     val notificationTitle = "Odometer Update"
     val notificationText = "Wassup bro! Update odometer?"
 
-    override fun onReceive(p0: Context?, p1: Intent?) {
+    override fun onReceive(p0: Context?, p1: Intent) {
+
+        val setHour: Int = p1.getIntExtra("HOUR_OF_DAY", -1)
+        val setMinute: Int = p1.getIntExtra("MINUTE", -1)
+        val rightNow: Calendar = Calendar.getInstance()
+        val currentHour: Int = rightNow.get(Calendar.HOUR_OF_DAY)
+        val currentMinute: Int = rightNow.get(Calendar.MINUTE)
+
+        println("currentHour: $currentHour")
+        println("setHour: $setHour")
+
+        println("currentMinute: $currentMinute")
+        println("setMinute: $setMinute")
 
 
+        if(currentHour == setHour && currentMinute == setMinute){
 
-        println("Alarm Triggered!")
+            println("Alarm Triggered!")
 
-        val intent = Intent(p0, AddOdometerActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(p0, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val intent = Intent(p0, AddOdometerActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(p0, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        var builder = NotificationCompat.Builder(p0!!, channelId)
-            .setSmallIcon(R.drawable.motorbroicon)
-            .setContentTitle(notificationTitle)
-            .setContentText(notificationText)
-            .setDefaults(Notification.DEFAULT_VIBRATE) //Important for heads-up notification
-            .setPriority(Notification.PRIORITY_MAX) //Important for heads-up notification
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
+            var builder = NotificationCompat.Builder(p0!!, channelId)
+                .setSmallIcon(R.drawable.motorbroicon)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationText)
+                .setDefaults(Notification.DEFAULT_VIBRATE) //Important for heads-up notification
+                .setPriority(Notification.PRIORITY_MAX) //Important for heads-up notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
 
-        val manager = p0!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val manager = p0!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        manager.notify(0, builder.build())
+            manager.notify(0, builder.build())
+
+        }else{
+
+            println("Alarm time not equal, don't show notification")
+
+        }
+
 
 
     }
--- app/src/main/java/com/elevintech/motorbro/ScheduledNotification/ScheduledNotification.kt
+++ app/src/main/java/com/elevintech/motorbro/ScheduledNotification/ScheduledNotification.kt
@@ -1,34 +1,45 @@
 package com.elevintech.motorbro.ScheduledNotification
 
-import android.app.*
-import android.content.BroadcastReceiver
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import com.elevintech.myapplication.R
+import io.opencensus.internal.Utils
 import java.util.*
 
 
 class ScheduledNotification {
+
+    val whatTimeToTriggerAlarm = 19 // 24 hour format = 7pm
+    val whatMinuteToTriggerAlarm = 0 //
 
     fun startAlarm(context: Context){
 
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
         val intent = Intent(context, Receiver::class.java)
+        intent.putExtra("HOUR_OF_DAY", whatTimeToTriggerAlarm);
+        intent.putExtra("MINUTE", whatMinuteToTriggerAlarm);
+
         val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         // Set the alarm to start at approximately 7:00 p.m.
         val calendar: Calendar = Calendar.getInstance().apply {
             timeInMillis = System.currentTimeMillis()
-            set(Calendar.HOUR_OF_DAY, 19)
+            set(Calendar.HOUR_OF_DAY, whatTimeToTriggerAlarm)
+            set(Calendar.MINUTE, whatMinuteToTriggerAlarm)
         }
 
-        println("Date Now: " + System.currentTimeMillis())
+
+/*        println("Date Now: " + System.currentTimeMillis())
         println("Alarm Date: " + calendar.timeInMillis)
+
+        val utils = com.elevintech.motorbro.Utils.Utils()
+
+        println("Date Now: " + utils.convertMillisecondsToDate(System.currentTimeMillis() / 1000, "MM/dd/yyyy hh:mm a"))
+        println("Alarm Date: " + utils.convertMillisecondsToDate(calendar.timeInMillis / 1000, "MM/dd/yyyy hh:mm a"))
+*/
+
 
         // Make alarm repeat everyday
         alarmManager.setInexactRepeating(
@@ -38,6 +49,19 @@ class ScheduledNotification {
             pendingIntent
         )
 
+
+        val t = System.currentTimeMillis();
+        if (t <= calendar.timeInMillis) {
+
+            println("alarm time not yet reached")
+
+
+        } else {
+
+            println("alarm has passed")
+
+
+        }
 
 
     }
